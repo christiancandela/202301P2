d36e249bc058881f216caec9433c18a72244fb4d
christiancandela <christiancandela@gmail.com>
2023-02-16 19:44:21 -0500

    Se adiciona atributo a la clase Sala

--- poo/cine/src/main/java/co/edu/uniquindio/ingesis/Sala.java
+++ poo/cine/src/main/java/co/edu/uniquindio/ingesis/Sala.java
@@ -1,4 +1,20 @@
 package co.edu.uniquindio.ingesis;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Sala {
+    private final Collection<Silla> sillas;
+    private final String nombre;
+
+    public Sala(String nombre) {
+        this.sillas = new ArrayList<>();
+        this.nombre = nombre;
+    }
+
+    public void adicionarSilla(TipoSilla tipoSilla,String posicion) throws Exception {
+
+        Silla silla = new Silla(tipoSilla,posicion);
+        sillas.add(silla);
+    }
 }
